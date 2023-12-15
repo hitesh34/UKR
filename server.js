0952fcd66f88d06d88083680acfcad9ab4e2a39c
hitesh34 <hiteshmansinghani@gmail.com>
2023-12-15 13:50:17 +0400

    update

--- server.js
+++ server.js
@@ -39,9 +39,10 @@ async function sendEmailWithPDF(pdfBuffer, subject, email) {
 }
 
 async function generatePDF(htmlContent) {
-const browser = await puppeteer.launch({
-    executablePath: 'C:/Program Files/Google/Chrome/Application/chrome.exe',
-  }); 
+  const browser = await puppeteer.launch({
+    executablePath: 'C:\Program Files\Google\Chrome\Application\chrome.exe',
+    args: ["--no-sandbox", "--disable-setuid-sandbox"]
+  });
    const page = await browser.newPage();
 
   const fullHTML = `
