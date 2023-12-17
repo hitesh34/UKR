4b4baca1e73ff277a07af3a8c12993d2c2b60a72
hitesh34 <hiteshmansinghani@gmail.com>
2023-12-17 23:45:15 +0400

    new

--- server.js
+++ server.js
@@ -40,8 +40,7 @@ async function sendEmailWithPDF(pdfBuffer, subject, email) {
 
 async function generatePDF(htmlContent) {
   const browser = await puppeteer.launch({
-    executablePath: '"C:/Program Files/Google/Chrome/Application/chrome.exe"', // Use the bundled Chromium executable
-    headless: false,
+    headless: true,
     args: ["--no-sandbox", "--disable-setuid-sandbox"]
   });
    const page = await browser.newPage();
